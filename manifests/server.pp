6de53b406b931d3d62223cd46beda54bf4950483
Guillaume Penaud <gpenaud@xilopix.com>
2015-10-19 15:41:44 +0200

    --amend

--- manifests/server.pp
+++ manifests/server.pp
@@ -200,36 +200,35 @@ define redis::server (
       File["/etc/redis_${redis_name}.conf"],
       File["${redis_dir}/redis_${redis_name}"]
     ],
-   notify  => [ Exec["systemd-enable_${redis_name}"], Service["redis-server_${redis_name}"] ],
- }
-
- # ----------------------------------------------------------------------------
-
-# install and configure logrotate
-if ! defined(Package['logrotate']) {
-  package { 'logrotate': ensure => installed; }
+  notify => [Exec["systemd-enable_${redis_name}"],Service["redis-server_${redis_name}"]],
 }
 
-file { "/etc/logrotate.d/redis-server_${redis_name}":
-   ensure  => file,
-   content => template('redis/redis_logrotate.conf.erb'),
-   require => [
-     Package['logrotate'],
-     File["/etc/redis_${redis_name}.conf"],
- ]
-}
+# ----------------------------------------------------------------------------
 
+  # install and configure logrotate
+  if ! defined(Package['logrotate']) {
+    package { 'logrotate': ensure => installed; }
+  }
 
- # ----------------------------------------------------------------------------
+  file { "/etc/logrotate.d/redis-server_${redis_name}":
+    ensure  => file,
+    content => template('redis/redis_logrotate.conf.erb'),
+    require => [
+      Package['logrotate'],
+      File["/etc/redis_${redis_name}.conf"],
+    ]
+  }
+
+# ----------------------------------------------------------------------------
 
   exec { "systemd-reload_${redis_name}":
     command   =>'systemctl daemon-reload',
-    path      => "/usr/local/bin/:/bin/";
+    path      => '/usr/local/bin/:/bin/';
   }
 
   exec { "systemd-enable_${redis_name}":
     command => "systemctl enable redis-server_${redis_name}",
-    path    => "/usr/local/bin/:/bin/",
+    path    => '/usr/local/bin/:/bin/',
     before  => Exec["systemd-reload_${redis_name}"];
   }
 
