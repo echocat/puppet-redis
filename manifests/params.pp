9fd2fed9427302d70149b8c6742ca3b2cf17ed70
Mathias Ewald <mewald@ebay.com>
2016-04-05 15:00:52 +0200

    added redis_download_base parameter

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,4 +8,5 @@ class redis::params {
   $download_tool         = 'curl -s -L'
   $redis_user            = undef
   $redis_group           = undef
+  $redis_download_base	 = 'http://download.redis.io'
 }
