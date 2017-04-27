084ec7e8d405e31cdb67f372fc02d9f65809447b
Dominic Vallejo <dominic.vallejo@riskiq.net>
2017-04-27 14:32:34 -0700

    dropping startup script in init.d for centos6 instead of systemd directory

--- manifests/server.pp
+++ manifests/server.pp
@@ -198,9 +198,14 @@ define redis::server (
   # startup script
   case $::operatingsystem {
     'Fedora', 'RedHat', 'CentOS', 'OEL', 'OracleLinux', 'Amazon', 'Scientific': {
+      if versioncmp($::operatingsystemmajrelease, '7') > 0 {
+      $has_systemd = true
       $service_file = "/usr/lib/systemd/system/redis-server_${redis_name}.service"
-      if versioncmp($::operatingsystemmajrelease, '7') > 0 { $has_systemd = true }
+    } else {
+      $has_systemd = false
+      $service_file = "/etc/init.d/redis-server_${redis_name}"
     }
+  }
     'Debian': {
       $service_file = "/etc/systemd/system/redis-server_${redis_name}.service"
       if versioncmp($::operatingsystemmajrelease, '8') > 0 { $has_systemd = true }
