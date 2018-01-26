c0483e66eab6a283a69a834b53c6de1624b216a8
joe <github.com@caleri.de>
2018-01-26 20:01:11 -0300

    fix OS version comparisons for use of systemd or init.d templates

--- manifests/server.pp
+++ manifests/server.pp
@@ -200,20 +200,32 @@ define redis::server (
   # startup script
   case $::operatingsystem {
     'Fedora', 'RedHat', 'CentOS', 'OEL', 'OracleLinux', 'Amazon', 'Scientific': {
-      $service_file = "/usr/lib/systemd/system/redis-server_${redis_name}.service"
-      if versioncmp($::operatingsystemmajrelease, '7') > 0 { $has_systemd = true }
+      if versioncmp($::operatingsystemmajrelease, '7') >= 0 {
+        $has_systemd = true
+        $service_file = "/usr/lib/systemd/system/redis-server_${redis_name}.service"
+      } else {
+        $service_file = "/etc/init.d/redis-server_${redis_name}"
+      }
     }
     'Debian': {
-      $service_file = "/etc/systemd/system/redis-server_${redis_name}.service"
-      if versioncmp($::operatingsystemmajrelease, '8') > 0 { $has_systemd = true }
+      if versioncmp($::operatingsystemmajrelease, '8') >= 0 {
+        $has_systemd = true
+        $service_file = "/etc/systemd/system/redis-server_${redis_name}.service"
+      } else {
+        $service_file = "/etc/init.d/redis-server_${redis_name}"
+      }
     }
     'Ubuntu': {
-      $service_file = "/etc/systemd/system/redis-server_${redis_name}.service"
-      if versioncmp($::operatingsystemmajrelease, '14.04') > 0 { $has_systemd = true }
+      if versioncmp($::operatingsystemmajrelease, '15.04') >= 0 {
+        $has_systemd = true
+        $service_file = "/etc/systemd/system/redis-server_${redis_name}.service"
+      } else {
+        $service_file = "/etc/init.d/redis-server_${redis_name}"
+      }
     }
     default:  {
-      $service_file = "/etc/init.d/redis-server_${redis_name}"
       $has_systemd = false
+      $service_file = "/etc/init.d/redis-server_${redis_name}"
     }
   }
 
